05492eab70c65720c2080a1805443238504b33af
Alex Kor <kav2006k@gmail.com>
2026-03-22 13:51:05 +0300

    Add User model and create_users migration
    Validations for name and email; unique index on email. Schema for users table.

---
+++ lesson-7/reports-app/app/models/user.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class User < ApplicationRecord
+  validates :first_name, presence: true, length: { maximum: 100 }
+  validates :last_name, presence: true, length: { maximum: 100 }
+  validates :email, presence: true, length: { maximum: 150 },
+                    format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  def full_name
+    "#{last_name} #{first_name}"
+  end
+end
